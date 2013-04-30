d031fcc8e7436a304f7b81b30ae13b4f08512b25
Scott and Cody B <scott+codyb@appacademy.io>
2013-04-30 09:27:31 -0400

    filled out card class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,5 +1,21 @@
-class Deck
+require "card"
 
+class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+
+    CARD.suits.each do |suit|
+      CARD.strings.each do |value|
+        @cards << Card.new(suit, value)
+      end
+    end
+  end
+
+  def count
+    @cards.count
+  end
 
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,24 @@
 require 'rspec'
 require 'deck'
+
+
+describe Deck do
+  let(:deck) {Deck.new}
+
+  describe "#initialize" do
+    it "initializes with all the cards" do
+      deck.count.should == 52
+    end
+  end
+
+  describe "#shuffle" do
+    pending
+  end
+
+  describe "#take(n)" do
+    pending
+    deck.take(2)
+    deck.count.should == 50
+  end
+
+end
